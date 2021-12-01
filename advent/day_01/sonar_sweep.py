9f9af524aa9bb336b5791bd311db72f2aa829490
Jan Doležal <jandolezal@email.cz>
2021-12-01 15:00:01 +0100

    Add solutions for day 1

---
+++ advent/day_01/sonar_sweep.py
@@ -0,0 +1,40 @@
+"""
+Day 1: Sonar Sweep
+https://adventofcode.com/2021/day/1
+"""
+
+
+def get_increase_count(data):
+    count = 0
+    for i in range(len(data) - 1):
+        if data[i+1] > data[i]:
+            count += 1
+    return count
+
+
+# def get_decrease_count(data):
+#     return sum(True if data[i+1] > data[i] else False for i in range(len(data) - 1))
+
+
+def get_window_increase_count(data, window_size=3):
+    count = 0
+    for i in range(len(data) - window_size):
+        first_sum = sum(data[i:i+window_size])
+        second_sum = sum(data[i+1:i+1+window_size])
+        # print(first_sum, second_sum)
+        if first_sum < second_sum:
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    with open('advent/day_01/input.txt') as f:
+        data = [int(word.strip()) for word in f.readlines()]
+
+    #1
+    print(get_increase_count(data))
+
+    #2
+    print(get_window_increase_count(data))
+
+    assert get_window_increase_count(data, 1) == get_increase_count(data)
---
+++ advent/day_01/test_sonar_sweep.py
@@ -0,0 +1,38 @@
+import unittest
+
+from advent.day_01.sonar_sweep import get_increase_count, get_window_increase_count
+
+
+with open('advent/day_01/test_input.txt') as f:
+    test_input = [int(word.strip()) for word in f.readlines()]
+
+test_output = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
+with open('advent/day_01/input.txt') as f:
+    data = [int(word.strip()) for word in f.readlines()]
+
+
+
+class TestSonarSweep(unittest.TestCase):
+
+
+    def test_sonar_sweep_with_test_data(self):
+        self.assertEqual(test_input, test_output)
+        increase_count = get_increase_count(test_input)
+        self.assertEqual(increase_count, 7)
+
+    def test_sonar_sweep(self):
+        increase_count = get_increase_count(data)
+        self.assertEqual(increase_count, 1466)
+    
+    def test_sonar_sweep_sliding_window_with_test_data(self):
+        increase_count = get_window_increase_count(test_input)
+        self.assertEqual(increase_count, 5)
+
+    def test_sonar_sweep_sliding_window(self):
+        increase_count = get_window_increase_count(data)
+        self.assertEqual(increase_count, 1491)
+
+
+if __name__ == '__main__':
+    unittest.main()
