e64bd120a3fd10419a810682139e9481510da403
Jan Doležal <jandolezal@email.cz>
2021-12-06 11:00:59 +0100

    Add part 2 solution for day 6

--- day_06/lanternfish.py
+++ day_06/lanternfish.py
@@ -2,11 +2,22 @@
 Day 6: Lanternfish
 https://adventofcode.com/2021/day/6
 """
+from collections import Counter
 
 
 def load_input(filepath):
     with open(filepath) as f:
         return [int(num) for num in f.read().split(',')]
+
+
+def load_input_as_counter(filepath):
+    with open(filepath) as f:
+        numbers = [int(num) for num in f.read().split(',')]
+        c = Counter(numbers)
+        for i in range(9):
+            if i not in c:
+                c[i] = 0
+        return c
 
 
 def simulate(fishes, days):
@@ -19,12 +30,29 @@ def simulate(fishes, days):
             else:
                 fishes[i] = fishes[i] - 1
         fishes.extend(new)
-        # print(f'{day}: {fishes}')
     return len(fishes), fishes
+
+
+def another_simulate(fishes, days):
+    for day in range(days):
+        for i in range(0,8):
+            if i == 0:
+                new_sixes = fishes[i]
+                fishes[i] = fishes[i+1]
+            else:
+                fishes[i] = fishes[i+1]
+        fishes[6] += new_sixes # update count of six
+        fishes[8] = new_sixes # add new fishes
+    return fishes.total()
 
 
 if __name__ == '__main__':
     # 1
-    fishes = load_input('day_06/test_input.txt')
-    count, new_fishes = simulate(fishes, 18)
+    fishes = load_input('day_06/input.txt')
+    count, new_fishes = simulate(fishes, 80)
     print(count)
+
+    # 2
+    fishes = load_input_as_counter('day_06/input.txt')
+    total = another_simulate(fishes, 256)
+    print(total)
--- day_06/test_lanternfish.py
+++ day_06/test_lanternfish.py
@@ -1,5 +1,7 @@
 import unittest
-from day_06.lanternfish import load_input, simulate
+from day_06.lanternfish import load_input, load_input_as_counter, simulate, another_simulate
+
+from array import array
 
 
 class TestLanternfish(unittest.TestCase):
@@ -7,7 +9,7 @@ class TestLanternfish(unittest.TestCase):
     def test_loading_input(self):
         test_input = load_input('day_06/test_input.txt')
         assert test_input == [3,4,3,1,2]
-    
+
     def test_simulate_with_test_input(self):
         test_input = load_input('day_06/test_input.txt')
         count, fishes = simulate(test_input, 18)
@@ -17,6 +19,17 @@ class TestLanternfish(unittest.TestCase):
         test_input = load_input('day_06/input.txt')
         count, fishes = simulate(test_input, 80)
         assert count == 365131
+    
+    def test_another_simulate_with_test_input(self):
+        test_input = load_input_as_counter('day_06/test_input.txt')
+        total = another_simulate(test_input, 256)
+        assert total == 26984457539
+
+    def test_another_simulate(self):
+        test_input = load_input_as_counter('day_06/input.txt')
+        total = another_simulate(test_input, 256)
+        assert total == 1650309278600
+
 
 if __name__ == '__main__':
     unittest.main()
