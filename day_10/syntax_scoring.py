2528f9c875158f359e78c7b66afccce201cd64f9
Jan Doležal <jandolezal@email.cz>
2021-12-10 14:49:50 +0100

    Add solution for part 2 on day 10

--- day_10/syntax_scoring.py
+++ day_10/syntax_scoring.py
@@ -3,6 +3,7 @@ Day 10: Syntax scoring
 https://adventofcode.com/2021/day/10
 """
 
+import statistics
 
 def load_input(filepath):
     with open(filepath) as f:
@@ -43,6 +44,19 @@ def find_illegal_closing(line):
         return first_char
 
 
+def mirror_opening(line):
+    map = {
+        '(': ')',
+        '[': ']',
+        '{': '}',
+        '<': '>',
+    }
+    try:
+        return ''.join([map[char] for char in line][::-1]) # reverse it
+    except KeyError: # this line is corrupted not incomplete, it has closing characters
+        return None
+
+
 if __name__ == '__main__':
     # 1
     data = load_input('day_10/input.txt')
@@ -58,3 +72,26 @@ if __name__ == '__main__':
     total = sum(points)
 
     print(total)
+
+    # 2
+    data = load_input('day_10/input.txt')
+
+    completions = []
+    for line in data:
+        opening_chars = reduce(line)
+        closing_chars = mirror_opening(opening_chars)
+        if closing_chars:
+            completions.append(closing_chars)
+
+    score_map = {')': 1, ']': 2, '}': 3, '>': 4}
+    scores = []
+
+    for chars in completions:
+        total = 0
+        for char in chars:
+            total = total * 5 + score_map[char]
+        scores.append(total)
+    
+    middle_score = statistics.median(scores)
+
+    print(middle_score)
