1567199e117a64591fd6a5b3f3507f1be28e8d7f
Jan Doležal <jandolezal@email.cz>
2021-12-13 20:50:39 +0100

    Add part 1 solution on day 13

---
+++ day_13/test_transparent_origami.py
@@ -0,0 +1,32 @@
+import unittest
+from day_13.transparent_origami import load_input, split, part1
+
+
+
+class TestTransparentOrigami(unittest.TestCase):
+    
+    def test_loading_input(self):
+        dots, instructions = load_input('day_13/test_input.txt')
+        assert len(dots) == 18
+        assert dots[0] == (3,0)
+        assert dots[-1] == (2,14)
+        assert len(instructions) == 2
+        assert instructions[-1] == ('x', 5)
+    
+
+    def test_part_1_with_test_data(self):
+        dots, instructions = load_input('day_13/test_input.txt')
+        new_dots = split(dots, 'y', 7)
+        assert len(new_dots) == 17
+
+        another_dots = split(new_dots, 'x', 5)
+        assert len(another_dots) == 16
+    
+
+    def test_part_1(self):
+        new_dots = part1()
+        assert len(new_dots) == 710
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ day_13/transparent_origami.py
@@ -0,0 +1,56 @@
+"""
+Day 13: Transparent Origami
+https://adventofcode.com/2021/day/13
+"""
+
+
+def load_input(filepath):
+    with open(filepath) as f:
+        lines = [line.strip() for line in f.readlines()]
+
+        empty_line_index = lines.index('')
+
+        dots = [(int(x), int(y)) for x,y in [line.split(',') for line in lines[:empty_line_index]]]
+        instructions_raw = [tuple(inst) for inst in [[part.strip('fold along ') for part in line.split('=')] for line in lines[empty_line_index+1:]]]
+        instructions = []
+        for axis, value in instructions_raw:
+            instructions.append((axis, int(value)))
+
+        return sorted(dots, key=lambda x: (x[1], x[0])), instructions
+
+
+def split(dots, axis, fold_index):
+
+    if axis == 'y':
+        first = [dot for dot in dots if dot[1] < fold_index]
+        second = [dot for dot in dots if dot[1] > fold_index]
+
+        new_second = []
+        for x, y in second:
+            new_y = y - ((y - fold_index) * 2) # new y index from the fold index
+            new_second.append((x, new_y))
+
+    elif axis == 'x':
+        first = [dot for dot in dots if dot[0] < fold_index]
+        second = [dot for dot in dots if dot[0] > fold_index]
+
+        new_second = []
+        for x, y in second:
+            new_x = x - ((x - fold_index) * 2) # same logic as for y axis
+            new_second.append((new_x, y))  
+
+    return list(set(first + new_second))
+
+
+def part1(filename='day_13/input.txt'):
+    dots, instructions = load_input(filename)
+    axis, fold_index = instructions[0] # only first line of instructions
+    new_dots = split(dots, axis, fold_index)
+    return new_dots
+
+
+if __name__ == '__main__':
+    # 1
+    new_dots = part1()
+    print(len(new_dots))
+ 
