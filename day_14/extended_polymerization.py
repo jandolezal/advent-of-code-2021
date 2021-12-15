55b759fc8eba6119ad8024e048fdf957103f9a7e
Jan Doležal <jandolezal@email.cz>
2021-12-15 21:13:30 +0100

    Change implementation to get to part 2 solution for day 14

--- day_14/extended_polymerization.py
+++ day_14/extended_polymerization.py
@@ -3,50 +3,82 @@ Day 14: Extended Polymerization
 https://adventofcode.com/2021/day/14
 """
 
-from array import array
 import collections
 import itertools
+
+
+def make_counter(template):
+    # https://stackoverflow.com/questions/5434891/iterate-a-list-as-pair-current-next-in-python
+    # https://stackoverflow.com/questions/61126284/zipped-python-generators-with-2nd-one-being-shorter-how-to-retrieve-element-tha
+    a, b = itertools.tee(template)
+    next(b, None)
+    return collections.Counter(list(zip(a, b)))
 
 
 def load_input(filepath='day_14/test_input.txt'):
     with open(filepath) as f:
         lines = [line.strip() for line in f.readlines()]
-        template = array('b', (ord(char)  for char in lines[0]))
-        rules = {(ord(k[0]), ord(k[1])) : ord(v) for k, v in [line.split(' -> ') for line in lines[2:]]}
+        template = make_counter(lines[0])
+        rules = {(k[0], k[1]) : v for k, v in [line.split(' -> ') for line in lines[2:]]}
         return template, rules
 
 
-def update_template(template, rules):
-    # https://stackoverflow.com/questions/5434891/iterate-a-list-as-pair-current-next-in-python
-    # https://stackoverflow.com/questions/61126284/zipped-python-generators-with-2nd-one-being-shorter-how-to-retrieve-element-tha
-    a, b = itertools.tee(template)
-    next(b, None)
+def update_counter(template, rules):
+    for k, v in collections.Counter(template).items():
+        first, second = k
+        new_element = rules[k]
+        # Add count of new pairs
+        template[(first, new_element)] += v
+        template[(new_element, second)] += v
+        # Former pairs are no more
+        template[k] -= v
 
-    elements = array('b', [])
 
-    for pair in zip(a, b):
-        elements.append(rules[pair])
-    
-    combined = array('b', (item for subtuple in zip(template, elements) for item in subtuple))
-    combined.append(template[-1]) # append last original element which is missing
-
-    return combined
-
-def make_steps(template, rules, steps=10, verbose=False):
+def make_steps(template, rules, steps=10):
     for step in range(steps):
-        template = update_template(template, rules)
-        if verbose:
-            print(f'Finished step {step}') # with: {"".join(chr(char) for char in template)}
-    return template
+        update_counter(template, rules)
+
+
+def from_pairs_to_individual_letters(template):
+    first = collections.defaultdict(int)
+    second = collections.defaultdict(int)
+    middle = collections.defaultdict(int)
+    individual = collections.defaultdict(int)
+
+    # Make two dictionaries to count separately first and second letters
+    # from pairs count
+    for k, v in template.items():
+        first[k[0]] += v
+        second[k[1]] += v
+
+    # Identify count of letters which overlap: letter is first and second in pair
+    for item in {**first, **second}:
+        middle[item] = min(first[item], second[item])
+    
+    # Make new dictionary finally with counts for individual letters
+    for k, v in template.items():
+        individual[k[0]] += v
+        individual[k[1]] += v
+
+    # Adjust letter count to reflect situation that letters were duplicated in former pair key
+    for k in individual:
+        individual[k] -= middle[k]
+
+    return individual
 
 
 if __name__ == '__main__':
-    # 1
+
+    # 1 & 2
     template, rules = load_input('day_14/input.txt')
+    
+    make_steps(template, rules, steps=40)
+    
+    # Convert pairs counts to individual letters counts
+    individual = from_pairs_to_individual_letters(template)
 
-    final_template = make_steps(template, rules, steps=10, verbose=True)
-
-    c = collections.Counter(final_template)
+    # Final calculation
+    c = collections.Counter(individual)
     most_common = c.most_common()
     most = most_common[0][1]
     least = most_common[-1][1]
--- day_14/test_extended_polymerization.py
+++ day_14/test_extended_polymerization.py
@@ -1,28 +1,15 @@
-from array import array
 import collections
 import unittest
 
 from day_14.extended_polymerization import load_input, make_steps
 
 
-
 class TestExtendedPolymerization(unittest.TestCase):
     
     def test_loading_input(self):
         template, rules = load_input('day_14/test_input.txt')
-        self.assertEqual(template, array('b', [78, 78, 67, 66]))
-        self.assertDictContainsSubset({(ord('C'), ord('H')): ord('B'), (ord('C'), ord('N')): ord('C')}, rules)
-    
-    def test_make_steps_with_test_input(self):
-        template, rules = load_input('day_14/test_input.txt')
-        final_template = make_steps(template, rules, steps=10)
-
-        c = collections.Counter(final_template)
-        most_common = c.most_common()
-        most = most_common[0][1]
-        least = most_common[-1][1]
-
-        self.assertEqual(most - least, 1588)
+        self.assertEqual(template, collections.Counter({('N', 'N'): 1, ('N', 'C'): 1, ('C', 'B'): 1}))
+        self.assertDictContainsSubset({('C', 'H'): 'B', ('C', 'N'): 'C'}, rules)
 
 
 if __name__ == '__main__':
