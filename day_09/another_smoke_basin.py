a6255e61b8c22b0bc40abfc9cb367437b23d673e
Jan Doležal <jandolezal@email.cz>
2021-12-16 15:52:46 +0100

    Use another approach for part 1 for day 9

--- day_09/another_smoke_basin.py
+++ day_09/another_smoke_basin.py
@@ -9,30 +9,21 @@ def load_input(filepath):
         return [[int(num) for num in line.strip()] for line in f.readlines()]
 
 
-def check_neighbours(row, col, heightmap):
-    neighbours = []
-
-    this_height = heightmap[row][col]
-    
+def all_neighbours_are_higher(row, col, heightmap):        
     rows = len(heightmap)
     cols = len(heightmap[0])
 
-    ok = [
-        (row - 1, col),
-        (row + 1, col),
-        (row, col - 1),
-        (row, col + 1),
-        ]
-    # Filter out illegal indices     
-    ok = [(row, col) for row, col in ok if (row >= 0) and (row < rows) and (col >= 0) and (col < cols)]
-    for row in range(rows):
-        for col in range(cols):
-            if (row, col) in ok:
-                neighbours.append(heightmap[row][col])
+    maybe = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
+    # Filter out illegal indices   
+    allowed_indices = [(row, col) for row, col in maybe if (row >= 0) and (row < rows) and (col >= 0) and (col < cols)]
 
-    return all(True if this_height < height else False for height in neighbours)
+    for adjacent_row, adjacent_col in allowed_indices:
+        if heightmap[row][col] >= heightmap[adjacent_row][adjacent_col]:
+            return False
+    return True
 
-def check_all_points(heightmap):
+
+def gather_heights(heightmap):
     rows = len(heightmap)
     cols = len(heightmap[0])
 
@@ -40,7 +31,7 @@ def check_all_points(heightmap):
 
     for row in range(rows):
         for col in range(cols):
-            if check_neighbours(row, col, heightmap):
+            if all_neighbours_are_higher(row, col, heightmap):
                 heights.append(heightmap[row][col])
 
     return heights
@@ -49,5 +40,6 @@ def check_all_points(heightmap):
 if __name__ == '__main__':
     # 1
     heightmap = load_input('day_09/input.txt')
-    low_points = check_all_points(heightmap)
-    print(sum(point + 1 for point in low_points))
+    heights = gather_heights(heightmap)
+    risk_level_sum = sum([level + 1 for level in heights])
+    print(f'Sum of risk levels: {risk_level_sum}')
